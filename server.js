0b413e2ef92c205890dff8068c1040c1e4741427
ayakase <wibu1892001@gmail.com>
2023-07-23 21:23:23 +0700

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000; // You can change the port number if needed
+const cors = require('cors')
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+app.get('/test', (req, res) => {
+    res.send('Hello from Express!');
+});
+app.get('/click', (req, res) => {
+    res.send('clicked from server')
+})
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
